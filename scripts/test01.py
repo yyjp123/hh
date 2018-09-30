69cd10607bfdb6573c75d341bb375aead42dfba1
xiaonanjp123 <824356606@qq.com>
2018-09-30 14:24:59 +0800

    新增03

--- scripts/test01.py
+++ scripts/test01.py
@@ -13,6 +13,11 @@ class Test01():
         print("2被执行")
         allure.attach("")
 
+    def test02(self):
+        allure.attach("")
+        print("2被执行")
+        allure.attach("")
+
     @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
     def test03(self):
         print("3被执行")
